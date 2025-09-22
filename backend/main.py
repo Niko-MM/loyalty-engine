c4db9a88906b0b18d83af0f0b669364f82ef9f6f
amphetam1ne <amphetam1ne@proton.me>
2025-09-22 17:12:18 +0200

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://niko-mm.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
